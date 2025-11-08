f65a6ee53eb4da78e774e91e87caed84a32965fd
ashirsarfraz <aashirsarfraz132@gmail.com>
2025-11-08 12:25:56 +0500

    Update workshop.php

--- .github/workshop.php
+++ .github/workshop.php
@@ -10,3 +10,28 @@ jobs:
 
     steps:
       - uses: actions/checkout@v4   
+      - name Barani CI/CD
+run:
+echo: "we are  learning  about devop's today"
+
+
+    steps:
+      - uses: step3   
+      - name Barani CI/CD
+run:
+echo: "we are  learning  about devop's today"
+
+
+    steps:
+      - uses:step2 
+      - name Barani CI/CD
+run:
+echo: "we are  learning  about devop's today"
+
+
+    steps:
+      - uses: step13
+      - name Barani CI/CD
+run:
+echo: "we are  learning  about devop's today"
+
